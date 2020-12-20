629a5688632c7a77531a39190f483182c93e030d
ivn <iwanaev23@gmail.com>
2020-12-20 22:25:02 +0300

    New entry point \'frmStart\'

--- Program.cs
+++ Program.cs
@@ -16,7 +16,7 @@ namespace Tinkoff_Бюджет
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new frmMainMenu());
+            Application.Run(new frmStart());
         }
     }
 }
