9ba8704ae727e98ce620dbe4a01954a5eb584219
ivn <iwanaev23@gmail.com>
2020-12-22 14:04:41 +0300

    Form authorization

---
+++ frmAuthorization.Designer.cs
@@ -0,0 +1,215 @@
+﻿
+namespace Tinkoff_Бюджет
+{
+    partial class frmStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && ( components != null )) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmStart));
+            this.labelLogin = new System.Windows.Forms.Label();
+            this.labelHeader = new System.Windows.Forms.Label();
+            this.panelUp = new System.Windows.Forms.Panel();
+            this.bttnClose = new System.Windows.Forms.PictureBox();
+            this.labelPassword = new System.Windows.Forms.Label();
+            this.tbLogin = new System.Windows.Forms.TextBox();
+            this.tbPassword = new System.Windows.Forms.TextBox();
+            this.bttnConfirm = new System.Windows.Forms.Button();
+            this.bttnCancel = new System.Windows.Forms.Button();
+            this.bttnRegistration = new System.Windows.Forms.Label();
+            this.panelUp.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bttnClose)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // labelLogin
+            // 
+            this.labelLogin.AutoSize = true;
+            this.labelLogin.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.labelLogin.Location = new System.Drawing.Point(12, 60);
+            this.labelLogin.Name = "labelLogin";
+            this.labelLogin.Size = new System.Drawing.Size(63, 21);
+            this.labelLogin.TabIndex = 8;
+            this.labelLogin.Text = "Логин:";
+            // 
+            // labelHeader
+            // 
+            this.labelHeader.AutoSize = true;
+            this.labelHeader.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelHeader.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.labelHeader.Location = new System.Drawing.Point(78, 8);
+            this.labelHeader.Name = "labelHeader";
+            this.labelHeader.Size = new System.Drawing.Size(122, 22);
+            this.labelHeader.TabIndex = 1;
+            this.labelHeader.Text = "Авторизация";
+            // 
+            // panelUp
+            // 
+            this.panelUp.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(230)))), ((int)(((byte)(13)))));
+            this.panelUp.Controls.Add(this.bttnClose);
+            this.panelUp.Controls.Add(this.labelHeader);
+            this.panelUp.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panelUp.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.panelUp.Location = new System.Drawing.Point(0, 0);
+            this.panelUp.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panelUp.Name = "panelUp";
+            this.panelUp.Size = new System.Drawing.Size(278, 38);
+            this.panelUp.TabIndex = 9;
+            // 
+            // bttnClose
+            // 
+            this.bttnClose.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttnClose.Image = ((System.Drawing.Image)(resources.GetObject("bttnClose.Image")));
+            this.bttnClose.Location = new System.Drawing.Point(1463, 2);
+            this.bttnClose.Margin = new System.Windows.Forms.Padding(0);
+            this.bttnClose.Name = "bttnClose";
+            this.bttnClose.Size = new System.Drawing.Size(35, 35);
+            this.bttnClose.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.bttnClose.TabIndex = 1;
+            this.bttnClose.TabStop = false;
+            // 
+            // labelPassword
+            // 
+            this.labelPassword.AutoSize = true;
+            this.labelPassword.Font = new System.Drawing.Font("Microsoft YaHei", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.labelPassword.Location = new System.Drawing.Point(12, 92);
+            this.labelPassword.Name = "labelPassword";
+            this.labelPassword.Size = new System.Drawing.Size(73, 21);
+            this.labelPassword.TabIndex = 10;
+            this.labelPassword.Text = "Пароль:";
+            // 
+            // tbLogin
+            // 
+            this.tbLogin.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(246)))));
+            this.tbLogin.Font = new System.Drawing.Font("Microsoft YaHei", 9F);
+            this.tbLogin.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.tbLogin.Location = new System.Drawing.Point(91, 61);
+            this.tbLogin.Name = "tbLogin";
+            this.tbLogin.Size = new System.Drawing.Size(171, 23);
+            this.tbLogin.TabIndex = 17;
+            // 
+            // tbPassword
+            // 
+            this.tbPassword.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(246)))));
+            this.tbPassword.Font = new System.Drawing.Font("Microsoft YaHei", 9F);
+            this.tbPassword.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.tbPassword.Location = new System.Drawing.Point(91, 93);
+            this.tbPassword.Name = "tbPassword";
+            this.tbPassword.Size = new System.Drawing.Size(171, 23);
+            this.tbPassword.TabIndex = 18;
+            this.tbPassword.UseSystemPasswordChar = true;
+            // 
+            // bttnConfirm
+            // 
+            this.bttnConfirm.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttnConfirm.FlatAppearance.BorderSize = 0;
+            this.bttnConfirm.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(236)))), ((int)(((byte)(72)))));
+            this.bttnConfirm.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(246)))));
+            this.bttnConfirm.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnConfirm.Font = new System.Drawing.Font("Microsoft YaHei", 9F);
+            this.bttnConfirm.Location = new System.Drawing.Point(1, 151);
+            this.bttnConfirm.Name = "bttnConfirm";
+            this.bttnConfirm.Size = new System.Drawing.Size(131, 30);
+            this.bttnConfirm.TabIndex = 23;
+            this.bttnConfirm.Text = "Войти";
+            this.bttnConfirm.UseVisualStyleBackColor = true;
+            this.bttnConfirm.Click += new System.EventHandler(this.bttnConfirm_Click);
+            // 
+            // bttnCancel
+            // 
+            this.bttnCancel.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttnCancel.FlatAppearance.BorderSize = 0;
+            this.bttnCancel.FlatAppearance.MouseDownBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(236)))), ((int)(((byte)(72)))));
+            this.bttnCancel.FlatAppearance.MouseOverBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(246)))));
+            this.bttnCancel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.bttnCancel.Font = new System.Drawing.Font("Microsoft YaHei", 9F);
+            this.bttnCancel.Location = new System.Drawing.Point(145, 151);
+            this.bttnCancel.Name = "bttnCancel";
+            this.bttnCancel.Size = new System.Drawing.Size(131, 30);
+            this.bttnCancel.TabIndex = 24;
+            this.bttnCancel.Text = "Отменить";
+            this.bttnCancel.UseVisualStyleBackColor = true;
+            this.bttnCancel.Click += new System.EventHandler(this.bttnCancel_Click);
+            // 
+            // bttnRegistration
+            // 
+            this.bttnRegistration.AutoSize = true;
+            this.bttnRegistration.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bttnRegistration.Font = new System.Drawing.Font("Microsoft YaHei", 9F);
+            this.bttnRegistration.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.bttnRegistration.Location = new System.Drawing.Point(178, 125);
+            this.bttnRegistration.Name = "bttnRegistration";
+            this.bttnRegistration.Size = new System.Drawing.Size(84, 17);
+            this.bttnRegistration.TabIndex = 25;
+            this.bttnRegistration.Text = "Регистрация";
+            this.bttnRegistration.Click += new System.EventHandler(this.bttnRegistration_Click);
+            this.bttnRegistration.MouseEnter += new System.EventHandler(this.bttnRegistration_MouseEnter);
+            this.bttnRegistration.MouseLeave += new System.EventHandler(this.bttnRegistration_MouseLeave);
+            // 
+            // frmStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(245)))), ((int)(((byte)(246)))));
+            this.ClientSize = new System.Drawing.Size(278, 191);
+            this.Controls.Add(this.bttnRegistration);
+            this.Controls.Add(this.bttnCancel);
+            this.Controls.Add(this.bttnConfirm);
+            this.Controls.Add(this.tbPassword);
+            this.Controls.Add(this.tbLogin);
+            this.Controls.Add(this.labelPassword);
+            this.Controls.Add(this.panelUp);
+            this.Controls.Add(this.labelLogin);
+            this.Font = new System.Drawing.Font("Microsoft YaHei", 8.25F);
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(51)))), ((int)(((byte)(51)))), ((int)(((byte)(51)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.Name = "frmStart";
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.panelUp.ResumeLayout(false);
+            this.panelUp.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.bttnClose)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelLogin;
+        private System.Windows.Forms.Label labelHeader;
+        private System.Windows.Forms.Panel panelUp;
+        private System.Windows.Forms.PictureBox bttnClose;
+        private System.Windows.Forms.Label labelPassword;
+        private System.Windows.Forms.TextBox tbLogin;
+        private System.Windows.Forms.TextBox tbPassword;
+        private System.Windows.Forms.Button bttnConfirm;
+        private System.Windows.Forms.Button bttnCancel;
+        private System.Windows.Forms.Label bttnRegistration;
+    }
+}
